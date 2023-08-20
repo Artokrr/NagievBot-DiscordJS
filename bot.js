4b0e25a4c3fed555c333da5506f82d5de9bb77e1
anivire <anivire.work@gmail.com>
2023-08-20 16:38:13 +0300

    🤔 change config to env

--- bot.js
+++ bot.js
@@ -1,11 +1,8 @@
-const { Client, Events, GatewayIntentBits, Partials, EmbedBuilder, ButtonBuilder, Component, ButtonStyle,
-    Embed, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, StreamType, AudioPlayerStatus,
-    entersState } = require('@discordjs/voice');
+const { Client, Events, GatewayIntentBits, Partials, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
+const { createAudioPlayer, NoSubscriberBehavior } = require('@discordjs/voice');
+require('dotenv').config();
 
-const config = require('./config.json');
-const token = config.token;
-const file = new AttachmentBuilder('./resources/audio/DmitriyNagiev.mp3')
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({
     intents: [
@@ -18,45 +15,24 @@ const client = new Client({
     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 })
 
-
 const player = createAudioPlayer({
     behaviors: {
         noSubscriber: NoSubscriberBehavior.Pause,
     },
 });
 
-
 client.once(Events.ClientReady, c => {
     console.log(`Нагиев готов. Залогинен как: ${c.user.tag}`);
 });
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
-    // // When a reaction is received, check if the structure is partial
-    // if (reaction.partial) {
-    //     try {
-    //         await reaction.fetch(); 
-    //     } catch (error) {
-    //         console.error('Something went wrong when fetching the message:', error);
-    //         return;
-    //     }
-    // }
     if (reaction.emoji.name === '📌' && reaction.message.channel.id === '622019376027271178') {
-        // Get the guild and the channel
-        console.log(`${user.username} закрепил сообщение`);
         const guild = client.guilds.cache.get('336164923983921163');
         const channel = guild.channels.cache.get('622019376027271178');
 
         // Get the message
         const pinMessageID = await channel.messages.fetch(reaction.message.id);
         const pinMessage = await channel.messages.fetch(reaction.message);
-        /* const regex = /(https?:\/\/(?:www\.)?youtu(?:\.be\/|be\.com\/(?:watch(?:\/|\/?\?(?:\S*&)?v=)|v\/|embed\/|user\/(?:[^/]+\#p\/u\/|[^/]+\/[^/]+\/)))([^"&?\/\s]{11}))/gi;
-        const matches = pinMessage.content.match(regex); */
-
-        /*  let url = null;
-         if (matches && matches.length > 0) {
-             url = matches[0];
-         } */
-
 
         // Check if it's the first pin reaction
         if (pinMessageID.reactions.cache.get('📌').count === 1) {
@@ -67,10 +43,6 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
                 .setColor('#2f3136')
                 .setAuthor({ name: pinMessage.author.username, iconURL: pinMessage.author.avatarURL() })
                 .setTimestamp(pinMessage.createdTimestamp);
-
-            // if (url) {
-            //     embed.setURL(url);
-            // }
 
             // Add content to embed
             if (pinMessage.content) {
@@ -92,8 +64,8 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
                  .setLabel('Удалить закреп')
                  .setStyle(ButtonStyle.Danger); */
 
-
             const row = new ActionRowBuilder().addComponents(redirectButton);
+
             // Send message
             await targetChannel.send({
                 embeds: [embed],
@@ -115,28 +87,6 @@ client.on(Events.MessageCreate, async message => {
     if (message.content.toLowerCase() === 'сколько стоит любовь') {
         message.channel.send('двадцать восемь мультов');
         console.log(`${message.author.username} использовал сколько стоит любовь`);
-    }
-    if (message.content.toLowerCase().includes('вы все гавно')) {
-        // const channel = message.member.voice.channel;
-        // const connection = joinVoiceChannel({
-        //     channelId: channel.id,
-        //     guildId: channel.guild.id,
-        //     adapterCreator: channel.guild.voiceAdapterCreator,
-        // });
-        // const resource = createAudioResource('./resources/audio/DmitriyNagiev.mp3');
-
-        //     player.play(resource);
-        //     await entersState(player, AudioPlayerStatus.Idle, 30e3);
-        //     player.on('error', error => {
-        //         console.error(`Ошибка в аудио плеере: ${error.message}`)
-        //     });
-        //     connection.subscribe(player);
-        //     // message.channel.send({ files: [file] });
-        message.channel.send({files: [file]});
-
-
-
-
     }
 });
 
